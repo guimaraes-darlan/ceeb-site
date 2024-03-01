5f77393f3dbfba12c376e21afe8df5beb371d948
Darlan Guimaraes <guimaraesdarlandev@gmail.com>
2024-03-01 16:17:43 -0300

    feat: escala de marco, texto home

--- src/app/escala-exposicoes/escala.tsx
+++ src/app/escala-exposicoes/escala.tsx
@@ -9,6 +9,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
 import TitlePage from "../../components/title-page";
 
@@ -24,18 +25,68 @@ export default function EscalaPage() {
   }
   const listScale: Scale[] = [
     {
-      day: "01 Quinta",
-      name: "Renan",
-      house: "Casa Espírita Eurípedes Barsanulpho",
-    },
-    {
-      day: "03 Sábado",
+      day: "02 Sábado",
       name: "Estudo do Evangelho",
       house: "Casa Espírita Eurípedes Barsanulpho",
     },
     {
-      day: "05 Sábado",
+      day: "04 Segunda",
       name: "Rosângela",
+      house: "Casa Espírita Eurípedes Barsanulpho",
+    },
+    {
+      day: "07 Quinta",
+      name: "Cláudio Luiz Mota",
+      house: "C. E. Portal da Vida",
+    },
+    {
+      day: "09 Sábado",
+      name: "Estudo do Evangelho",
+      house: "Casa Espírita Eurípedes Barsanulpho",
+    },
+    {
+      day: "11 Segunda",
+      name: "Nilson Góes",
+      house: " - ",
+    },
+    {
+      day: "14 Quinta",
+      name: "Letícia",
+      house: "Casa Espírita Eurípedes Barsanulpho",
+    },
+    {
+      day: "16 Sábado",
+      name: "Estudo do Evangelho",
+      house: "Casa Espírita Eurípedes Barsanulpho",
+    },
+    {
+      day: "18 Segunda",
+      name: "Benício Góes",
+      house: "C. E. Seara do Amor",
+    },
+    {
+      day: "21 Quinta",
+      name: "Edison Victor Faccin",
+      house: "C. E. Manoel Francisco da Luz",
+    },
+    {
+      day: "23 Sábado",
+      name: "Estudo do Evangelho",
+      house: "Casa Espírita Eurípedes Barsanulpho",
+    },
+    {
+      day: "25 Segunda",
+      name: "Sílvia Silva",
+      house: "C. E. Paulo de Tarso",
+    },
+    {
+      day: "28 Quinta",
+      name: "Renan",
+      house: "Casa Espírita Eurípedes Barsanulpho",
+    },
+    {
+      day: "23 Sábado",
+      name: "Estudo do Evangelho",
       house: "Casa Espírita Eurípedes Barsanulpho",
     },
   ];
@@ -48,6 +99,9 @@ export default function EscalaPage() {
       flexDirection="column"
     >
       <TitlePage title="Escala de Exposições Doutrinárias" />
+      <Typography variant="h6">
+        <strong>Março de 2024</strong>
+      </Typography>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
--- src/app/home-page.tsx
+++ src/app/home-page.tsx
@@ -16,9 +16,7 @@ export default function HomePage() {
       <Typography variant="h4" fontWeight="bold">
         Seja Bem Vindo!
       </Typography>
-      <Container sx={{ height: "2rem" }} />
-
-      <Container sx={{ height: "0.5rem" }} />
+      <Container sx={{ height: "1rem" }} />
       <Container
         sx={{
           width: {
@@ -27,13 +25,19 @@ export default function HomePage() {
           },
         }}
       >
-        <Typography align="center">
-          A Casa Espírita Eurípedes Barsanulpho, fundada em 29 de agosto de
-          2014, designada “CEEB”, é uma organização religiosa, de caráter
-          educacional, cultural, de assistência social, filantrópica, sem fins
-          lucrativos.
+        <Typography align="center" variant="h6">
+          <p>
+            A <strong>Casa Espírita Eurípedes Barsanulpho</strong>, fundada em
+            29 de agosto de 2014, é uma organização religiosa, de caráter
+            educacional, cultural, de assistência social, filantrópica, sem fins
+            lucrativos, e tem por objetivos a prática da caridade espiritual,
+            moral e material por todos os meios ao seu alcance e dentro dos
+            princípios da Doutrina Espírita codificada por Allan Kardec.
+          </p>
+          Conheça as nossas atividades e horários de funcionamento e seja muito
+          bem-vindo
         </Typography>
-        <Container sx={{ height: "4rem" }} />
+        <Container sx={{ height: "2rem" }} />
         <Grid container spacing={1}>
           <Grid item xs={12} sm={6} md={6}>
             <Image
