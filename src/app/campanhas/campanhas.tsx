66407c00a4bddd27ebafcc71471fd254afc1e4e0
Darlan Guimaraes <guimaraesdarlandev@gmail.com>
2024-05-05 18:02:15 -0300

    feat: doacoes

--- src/app/campanhas/campanhas.tsx
+++ src/app/campanhas/campanhas.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { Box, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Divider,
+  Grid,
+  ImageList,
+  ImageListItem,
+  Typography,
+} from "@mui/material";
 import TitlePage from "../../components/title-page";
 import Image from "next/image";
 
@@ -46,6 +54,119 @@ export default function CampanhasPage() {
         <Typography>- Leite</Typography>
         <Typography>- Café</Typography>
         <Typography>- Biscoito</Typography>
+      </Container>
+      <Container
+        sx={{
+          width: {
+            sx: "100%",
+            md: "70%",
+            marginTop: "1rem",
+          },
+        }}
+      >
+        <Divider />
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Typography fontWeight="bold" variant="h5">
+            Entrega de donativos (21/04/2024)
+          </Typography>
+          <Typography fontWeight="bold" variant="h6">
+            Agradecemos a todos pelas doações
+          </Typography>
+        </Box>
+        <ImageList
+          sx={{
+            width: "100%",
+          }}
+          cols={2}
+          rowHeight={300}
+        >
+          <ImageListItem>
+            <Image
+              src="/images/doca/img-1.jpg"
+              layout="fill"
+              objectFit="contain"
+              alt="img-1"
+              style={{ maxWidth: "400px" }}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <Image
+              src="/images/doca/img-3.jpg"
+              layout="fill"
+              objectFit="contain"
+              alt="img-3"
+              style={{ maxWidth: "400px" }}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <Image
+              src="/images/doca/img-4.jpg"
+              layout="fill"
+              objectFit="contain"
+              alt="img-4"
+              style={{ maxWidth: "400px" }}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <Image
+              src="/images/doca/img-5.jpg"
+              layout="fill"
+              objectFit="contain"
+              alt="img-5"
+              style={{ maxWidth: "400px" }}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <Image
+              src="/images/doca/img-6.jpg"
+              layout="fill"
+              objectFit="contain"
+              alt="img-6"
+              style={{ maxWidth: "400px" }}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <Image
+              src="/images/doca/img-7.jpg"
+              layout="fill"
+              objectFit="contain"
+              alt="img-7"
+              style={{ maxWidth: "400px" }}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <Image
+              src="/images/doca/img-8.jpg"
+              layout="fill"
+              objectFit="contain"
+              alt="img-8"
+              style={{ maxWidth: "400px" }}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <Image
+              src="/images/doca/img-9.jpg"
+              layout="fill"
+              objectFit="contain"
+              alt="img-9"
+              style={{ maxWidth: "400px" }}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <Image
+              src="/images/doca/img-10.jpg"
+              layout="fill"
+              objectFit="contain"
+              alt="img-10"
+              style={{ maxWidth: "400px" }}
+            />
+          </ImageListItem>
+        </ImageList>
       </Container>
     </Box>
   );
