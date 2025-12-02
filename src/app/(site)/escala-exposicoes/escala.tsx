4ecd1a335c794b2f2181f6e56371494eefb0e13c
Darlan Guimaraes <guimaraesdarlandev@gmail.com>
2025-12-02 20:44:05 -0300

    feat: mensagem de recesso

--- src/app/(site)/escala-exposicoes/escala.tsx
+++ src/app/(site)/escala-exposicoes/escala.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import TitlePage from "../../../components/title-page";
 import { getMonth } from "../../../util/dateUtil";
 import { CardCalendar } from "../../../components/cardCalendar";
@@ -25,6 +25,23 @@ export default function EscalaPage({ lectures }: EscalaPageProps) {
           {getMonth(actualDate)} de {actualDate.getFullYear()}
         </strong>
       </Typography>
+      <Box
+        sx={{
+          border: "2px solid #1976d2",
+          borderRadius: "8px",
+          padding: "1.5rem",
+          backgroundColor: "#f5faff",
+        }}
+      >
+        <Typography align="center" variant="h5" fontWeight="bold">
+          Estaremos em recesso entre os dias 14/12/2025 e 18/01/2026.
+        </Typography>
+        <Container sx={{ height: "1rem" }} />
+        <Typography align="center" variant="h5" fontWeight="bold">
+          Retorno das atividades em 19/01/2026.
+        </Typography>
+      </Box>
+      <Container sx={{ height: "2rem" }} />
       {lectures.map((lecture) => (
         <CardCalendar key={lecture.id} lecture={lecture} />
       ))}
